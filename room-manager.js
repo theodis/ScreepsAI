19587e45a2ef3cc3cef7386fe049fd18b47aa6cf
Mike Schmid <theodis3@gmail.com>
2018-01-28 12:03:27 -0800

    Removed some old commented code

--- room-manager.js
+++ room-manager.js
@@ -353,11 +353,3 @@ Object.defineProperty(Room.prototype, 'mine', {
 	configurable: true
 });
 
-/*module.exports.new = (room) => {
-	if(!(room instanceof Room)) throw room + " isn't a room!";
-	if(room.controller.owner.username != global.username) throw room + " isn't owned by current player!";
-	let ret = {
-	}
-	return ret;
-}*/
-
