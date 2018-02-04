051665e47430825f0110ed5e364de3664a902203
Mike Schmid <theodis3@gmail.com>
2018-02-03 20:56:24 -0800

    Allow specifying minimum distance for mini move task

--- creep.task.mini.move.js
+++ creep.task.mini.move.js
@@ -7,23 +7,26 @@ module.exports = {
 	},
 	run: function(task) {
 		let target = null;
+		let pos = null;
+		let mindist = task.mindist || 0;
+
 		let action_params = task.action_params || [];
 		if(task.target_id) target = Game.getObjectById(task.target_id);
+		if(target) pos = target.pos; else pos = task;
+
 		//If the action can be done then it's close enough
 		if(target && task.action){
 			let result = this[task.action](target, ...action_params);
 			if(result === OK || result === ERR_FULL || result == ERR_INVALID_TARGET) return "done";
 		}
+
+		let dist = Math.max(Math.abs(this.pos.x - pos.x), Math.abs(this.pos.y - pos.y));
 		//If at the destination we're done
-		if(
-			(target && target.pos.x === this.pos.x && target.pos.y === this.pos.y) ||
-			(task.x === this.pos.x && task.y === this.pos.y)
-		) return "done";
+		if( dist <= mindist ) return "done";
 
 		//Otherwise move to target
 		let result = null
-		if(target) result = this.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-		else result = this.moveTo(task.x, task.y, {visualizePathStyle: {stroke: '#ffffff'}});
+		result = this.moveTo(pos.x, pos.y, {visualizePathStyle: {stroke: '#ffffff'}});
 		task.last_result = result;
 		if(result === ERR_NO_PATH || result === ERR_NO_BODYPART || result == ERR_INVALID_TARGET) return "fail";
 		return "continue;"
