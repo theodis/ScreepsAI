6ad8b5bbf64a3dcee12730e17201a655bf5cf4dd
Mike Schmid <theodis3@gmail.com>
2018-02-09 11:39:16 -0800

    Early support for cross room distances

--- utility.js
+++ utility.js
@@ -9,7 +9,10 @@ global.getCPUCost = function(f) {
 global.distance = function(a,b) {
 	let pos_a = a.pos || a;
 	let pos_b = b.pos || b;
-	return Math.max(Math.abs(pos_a.x - pos_b.x), Math.abs(pos_a.y - pos_b.y));
+
+	if(pos_a.room === pos_b.room)
+		return Math.max(Math.abs(pos_a.x - pos_b.x), Math.abs(pos_a.y - pos_b.y));
+	return 9999 // TODO generate better cross room distance values
 }
 
 global.getNearest = function(pos, targets) {
