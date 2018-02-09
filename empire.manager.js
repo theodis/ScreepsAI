c079e663db567666f1f3bf1ec73b18d9cdc0aca9
Mike Schmid <theodis3@gmail.com>
2018-02-09 11:39:38 -0800

    Empire code work

--- empire.manager.js
+++ empire.manager.js
@@ -1,3 +1,19 @@
 Empire = {};
 
 require('empire.manager.recycler');
+
+Empire.run = function() {
+	Empire.recycle();
+}
+
+Object.defineProperty(Empire, 'mainSpawn', {
+	get: function() {
+		let mainSpawn = () => Object.keys(Game.rooms)
+					.map(key => Game.rooms[key])
+					.reduce((accumulator, currentValue) => accumulator === null ? currentValue : (currentValue === null ? accumulator : (currentValue.energyCapacityAvailable > accumulator.energyCapacityAvailable ? currentValue : accumulator)))
+					.mainSpawn;
+		return Memoize.get("mainSpawn", mainSpawn, undefined, 100);
+	},
+	enumerable: false,
+	configurable: true
+});
--- main.js
+++ main.js
@@ -13,8 +13,8 @@ module.exports.loop = function () {
 	//Keep bucket to a minimum of 100
 	if(Game.cpu.bucket < 100) return;
 
+	Empire.run();
 	Object.keys(Game.rooms).forEach(key => Game.rooms[key].run());
 	Object.keys(Game.creeps).forEach(key => Game.creeps[key].run());
-	Empire.recycle();
 }
 
